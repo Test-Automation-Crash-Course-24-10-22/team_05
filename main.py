e1a25269e7bb51a27b0040f51cbcc1c9d6df28c7
YuriiLemon <chempachannel@gmail.com>
2022-12-23 13:11:40 +0200

    other

--- main.py
+++ main.py
@@ -2,7 +2,4 @@ import time
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('https://rozetka.com.ua/ua/asus-90nb0ty1-m00vf0/p346597995/')
-button = browser.find_element(By.ID, "rzintersectionobserver")
-
-button.click()
 time.sleep(5)
