f4019325eb63e1ad754f7717d4119433ec300b77
YuriiLemon <chempachannel@gmail.com>
2022-12-27 21:56:05 +0200

    organized code in functions

--- chemerys_tests/tests/button_for_topics.py
+++ chemerys_tests/tests/button_for_topics.py
@@ -5,11 +5,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-browser.get('https://rozetka.com.ua/ua/asus-90nb0ty1-m00vf0/p346597995/')
-browser.execute_script("window.scrollTo(0, 1700);")
-time.sleep(5)
+def scroll():
+    browser.get('https://rozetka.com.ua/ua/asus-90nb0ty1-m00vf0/p346597995/')
+    browser.execute_script("window.scrollTo(0, 1700);")
+    time.sleep(5)
 
-button_for_topics = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-lazy-render/div/button')
-button_for_topics.click()
-time.sleep(5)
+def button_click():
+    button_for_topics = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-lazy-render/div/button')
+    button_for_topics.click()
+    time.sleep(5)
+
+scroll()
+button_click()
 
--- chemerys_tests/tests/undordered_list.py
+++ chemerys_tests/tests/undordered_list.py
@@ -5,42 +5,50 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-browser.get('https://rozetka.com.ua/ua/acer-nxa8eeu002/p357787248/')
-browser.execute_script("window.scrollTo(0, 600);")
-time.sleep(2)
+def scroll():
+    browser.get('https://rozetka.com.ua/ua/acer-nxa8eeu002/p357787248/')
+    browser.execute_script("window.scrollTo(0, 600);")
+    time.sleep(2)
 
-ul_button_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/label')
-ul_button_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/label')
+def unordered_list_buttons():
+    ul_button_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/label')
+    ul_button_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/label')
 
-ul_buttons = [ul_button_1, ul_button_2]
+    ul_buttons = [ul_button_1, ul_button_2]
+    for x in ul_buttons:
+        x.click()
+        time.sleep(1)
+        x.click()
+        time.sleep(1)
 
-for x in ul_buttons:
-    x.click()
-    time.sleep(1)
-    x.click()
-    time.sleep(1)
+def unordered_list_options():
+    ul_option_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/ul/li[1]/label')
+    ul_option_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/ul/li[2]/label')
+    ul_option_3 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/ul/li/label')
+    ul_option_4 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/ul/li[2]/label')
 
-ul_option_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/ul/li[1]/label')
-ul_option_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/ul/li[2]/label')
-ul_option_3 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/ul/li/label')
-ul_option_4 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/ul/li[2]/label')
+    ul_options = [ul_option_1, ul_option_2, ul_option_3, ul_option_4]
+    for y in ul_options:
+        y.click()
+        time.sleep(1)
 
-ul_options = [ul_option_1, ul_option_2, ul_option_3, ul_option_4]
+def information_buttons():
+    information_button_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/label/button')
+    information_button_1.click()
+    time.sleep(2)
+    exit_button_1 = browser.find_element(By.XPATH, '/html/body/app-root/rz-single-modal-window/div[3]/div[1]/button')
+    exit_button_1.click()
+    time.sleep(2)
 
-for y in ul_options:
-    y.click()
-    time.sleep(1)
+    information_button_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/label/button')
+    information_button_2.click()
+    time.sleep(2)
+    exit_button_2 = browser.find_element(By.XPATH, '/html/body/app-root/rz-single-modal-window/div[3]/div[1]/button')
+    exit_button_2.click()
+    time.sleep(2)
 
-information_button_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[1]/rz-service-item/label/button')
-information_button_1.click()
-time.sleep(2)
-exit_button_1 = browser.find_element(By.XPATH, '/html/body/app-root/rz-single-modal-window/div[3]/div[1]/button')
-exit_button_1.click()
-time.sleep(2)
+scroll()
+unordered_list_buttons()
+unordered_list_options()
+information_buttons()
 
-information_button_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[2]/rz-product-main-info/rz-additional-services/div/div/ul/li[2]/rz-service-item/label/button')
-information_button_2.click()
-time.sleep(2)
-exit_button_2 = browser.find_element(By.XPATH, '/html/body/app-root/rz-single-modal-window/div[3]/div[1]/button')
-exit_button_2.click()
-time.sleep(2)
