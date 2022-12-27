b419340ad3c51b600f68f1b0ef928902ab9fc0ab
YuriiLemon <chempachannel@gmail.com>
2022-12-27 21:32:01 +0200

    xpaths instead of locators

--- chemerys_tests/tests/big_carousel.py
+++ chemerys_tests/tests/big_carousel.py
@@ -9,14 +9,14 @@ browser.get('https://rozetka.com.ua/ua/asus-90nb0ty1-m00vf0/p346597995/')
 browser.execute_script("window.scrollTo(0, 200);")
 time.sleep(2)
 
-right_arrow = browser.find_element(By.CLASS_NAME, 'simple-slider__control--next')
+right_arrow = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[1]/div[1]/button[2]')
 x = 0
 while x < 14:
     right_arrow.click()
     time.sleep(1)
     x += 1
 
-left_arrow = browser.find_element(By.CLASS_NAME, 'simple-slider__control--prev')
+left_arrow = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[1]/div[1]/button[1]')
 y = 0
 while y < 14:
     left_arrow.click()
--- chemerys_tests/tests/button_for_topics.py
+++ chemerys_tests/tests/button_for_topics.py
@@ -6,10 +6,10 @@ from selenium.webdriver.common.by import By
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 browser.get('https://rozetka.com.ua/ua/asus-90nb0ty1-m00vf0/p346597995/')
-browser.execute_script("window.scrollTo(0, 700);")
+browser.execute_script("window.scrollTo(0, 1700);")
 time.sleep(5)
 
-button_for_topics = browser.find_element(By.CLASS_NAME, 'product-about__description-anchor')
+button_for_topics = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-lazy-render/div/button')
 button_for_topics.click()
 time.sleep(5)
 
