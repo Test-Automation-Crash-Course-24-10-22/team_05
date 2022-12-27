4ba6a7f4aa526485dea330b18b98bb7ec06f4ce9
YuriiLemon <chempachannel@gmail.com>
2022-12-27 21:09:08 +0200

    other

--- chemerys_tests/tests/small_carousel.py
+++ chemerys_tests/tests/small_carousel.py
@@ -16,6 +16,7 @@ left_arrow = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1
 left_arrow.click()
 time.sleep(2)
 
+slide_0 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[1]/rz-gallery-main-thumbnail-image/button')
 slide_1 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[1]/rz-gallery-main-thumbnail-image/button')
 slide_2 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[2]/rz-gallery-main-thumbnail-image/button')
 slide_3 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[3]/rz-gallery-main-thumbnail-image/button')
@@ -31,7 +32,6 @@ slide_12 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/
 slide_13 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[13]/rz-gallery-main-thumbnail-image/button')
 slide_14 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[14]/rz-gallery-main-thumbnail-image/button')
 slide_15 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[15]/rz-gallery-main-thumbnail-image/button')
-slide_0 = browser.find_element(By.XPATH, '//*[@id="#scrollArea"]/div[1]/div[1]/div/rz-product-gallery-main/app-slider[2]/div/div/ul/li[1]/rz-gallery-main-thumbnail-image/button')
 
 list = [slide_1, slide_2, slide_3, slide_4, slide_5, slide_6, slide_7, slide_8, slide_9, slide_10, slide_11, slide_12,
         slide_13, slide_14, slide_15]
